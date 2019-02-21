19ee54ea7dda05476ce1e8f6a0270c78256144e6
Eugenia Oshurko <yarutoua@gmail.com>
2019-02-21 18:34:42 +0100

    Finished forward/backward feeding

--- src/deeplearn.erl
+++ src/deeplearn.erl
@@ -1,6 +1,6 @@
 -module(deeplearn).
--export([synapse_loop/1, init_synapses/1, sigmoid/1, sigmoid_prime/1,
-		 tanh/1, tanh_prime/1, spawn_nn/5, init_neuron_loop/3, neuron_loop/5]).
+-export([synapse_loop/2, sigmoid/1, sigmoid_prime/1,
+		 tanh/1, tanh_prime/1, spawn_nn/6, init_neuron_loop/4, neuron_loop/5]).
 
 
 % Activation functions
@@ -10,16 +10,18 @@ sigmoid_prime(X) -> sigmoid(X) * (1 - sigmoid(X)).
 tanh(X) -> 2 * sigmoid(2 * X) - 1.
 tanh_prime(X) -> 1 - tanh(X) * tanh(X).
 
+activate(Value, sigmoid) -> sigmoid(Value);
+activate(Value, tanh) -> tanh(Value).
 
-activate(Weights, Values, Activation) ->
-	Val = lists:foldl(
-		fun(X, Y) -> X + Y end,
-		0,
-		lists:zipwith(fun(X, Y) -> X * Y end, [1.0 | Values], Weights)),
-	case Activation of
-		sigmoid -> sigmoid(Val);
-		tanh -> tanh(Val)
-	end.
+derivative(Value, sigmoid) -> sigmoid_prime(Value);
+derivative(Value, tanh) -> tanh_prime(Value).
+
+
+fold_with_weights(Values, Weights) ->
+	lists:foldl(
+		fun(X, Y) -> X + Y end, 0,
+		lists:zipwith(
+			fun(X, Y) -> X * Y end, [1.0 | Values], Weights)).
 
 % Activation loop of a neuron waiting for all input values to be received
 activation_loop(InNeuron, Value, InNeurons) ->
@@ -45,113 +47,249 @@ activation_loop(InNeuron, Value, InNeurons) ->
 		end,
 		InNeurons).
 
+% Error loop of a neuron waiting for all error values to be received
+error_loop(OutNeuron, Error, OutNeurons) ->
+	lists:map(
+		fun(OtherOutNeuron) ->
+			case OtherOutNeuron of 
+				OutNeuron -> Error;
+				_ ->
+					receive
+						{error, OtherOutNeuron, OtherError} ->
+							io:format("[~p] Received error ~w from ~p!~n", [self(), OtherError, OtherOutNeuron]),
+							OtherError;
+						stop_input -> 
+							io:format("Stoping error estimation loop~n", []),
+							stop
+					end
+			end
+		end,
+		OutNeurons).
 
-synapse_loop(OutNeurons) ->
+% Loop of an input neuron sending its value to out neurons
+% when receives the message {input, Value}
+synapse_loop(InputMonitor, OutNeurons) ->
 	receive
 		{out, NewOutNeurons} ->
 			io:format("[~p] New synapse output ~w ~n", [self(), NewOutNeurons]),
-			synapse_loop(NewOutNeurons);
-		{input, Value} ->
+			synapse_loop(InputMonitor, NewOutNeurons);
+		{input, InputMonitor, Value} ->
 			io:format("[~p] New synapse input ~w ~n", [self(), Value]),
 			lists:map(
 				fun(X) -> X ! {input, self(), Value} end,
 				OutNeurons),
-			synapse_loop(OutNeurons);
+			synapse_loop(InputMonitor, OutNeurons);
 		stop_input ->
 			io:format("Force activation stop ~n", []),
 			lists:map(
 				fun(X) -> X ! stop_input end,
 				OutNeurons),
-			synapse_loop(OutNeurons);
+			synapse_loop(InputMonitor, OutNeurons);
 		stop -> ok
 	end.
 
-init_synapses(N) ->
-	init_synapses(N, []).
-
-init_synapses(0, Synapses) ->
-	lists:reverse(Synapses);
-init_synapses(N, Synapses) ->
-	init_synapses(
-		N - 1,
-		[spawn(deeplearn, synapse_loop, [[]]) | Synapses]).
-
-
-neuron_loop(Monitor, InNeurons, OutNeurons, Weights, Activation) ->
+% Loop of an output neuron sending its value to out neurons
+% when receives the message {input, Value}
+output_loop(InNeurons, OutputMonitor,
+			Weights, Activation, InputValues) ->
 	% check whether monitor node is alive
-	case net_adm:ping(Monitor) of
-		pong -> 
-			ok;
-		pang ->
-			io:format("CANNOT CONNECT TO THE MONITOR: ~p!~n", [Monitor]),
-			neuron_loop(Monitor, InNeurons, OutNeurons, Weights, Activation)
-	end,
+	% io:format("[~p] Entered output neuron loop~n", [self()]),
 	receive
 		{input, InNeuron, Value} -> 
 			io:format("[~p] Received input ~w from ~p!~n", [self(), Value, InNeuron]),
-			Values = activation_loop(InNeuron, Value, InNeurons),
-			case lists:member(stop, Values) of
+			NewInputValues = activation_loop(InNeuron, Value, InNeurons),
+			case lists:member(stop, NewInputValues) of
 				false -> 
-					ActivationValue = activate(Weights, Values, Activation),
+					WeightedInput = fold_with_weights(NewInputValues, Weights),
+					ActivationValue = activate(WeightedInput, Activation),
 					io:format("[~p] Activated successfully with ~p!~n", [self(), ActivationValue]),
-					lists:map(
-						fun(X) -> X ! {input, self(), ActivationValue} end,
-						OutNeurons);
-					% io:format("[~p] will send to ~w~n", [self(), OutNeurons]);
+					OutputMonitor ! {output, self(), ActivationValue},
+					output_loop(InNeurons, OutputMonitor, Weights, Activation, NewInputValues);
 				_ -> 
 					io:format("Activation of ~p failed!~n", [self()]),
 					not_activated
 			end,
-			neuron_loop(Monitor, InNeurons, OutNeurons, Weights, Activation);
+			output_loop(InNeurons, OutputMonitor, Weights, Activation, InputValues);
+		{backpropagate, Prediction} ->
+			WeightedInput = fold_with_weights(InputValues, Weights),
+			Error = (activate(WeightedInput, Activation) - Prediction) * derivative(WeightedInput, Activation),
+			io:format("->> Error ~w~n", [Error]),
+			lists:zipwith(
+				fun(InNeuron, Weight) -> 
+					InNeuron ! {error, self(), Error * Weight}
+				end, 
+				InNeurons, tl(Weights)),
+			output_loop(InNeurons, OutputMonitor, Weights, Activation, InputValues)
+	end,
+	output_loop(InNeurons, OutputMonitor, Weights, Activation, InputValues).
+
+
+neuron_loop(InNeurons, OutNeurons, Weights, Activation, InputValues) ->
+	% io:format("[~p] Entered hidden neuron loop, out neurons ~w~n", [self(), OutNeurons]),
+	receive
+		{input, InNeuron, Value} -> 
+			io:format("[~p] Received input ~w from ~p!~n", [self(), Value, InNeuron]),
+			NewInputValues = activation_loop(InNeuron, Value, InNeurons),
+			case lists:member(stop, NewInputValues) of
+				false -> 
+					WeightedInput = fold_with_weights(NewInputValues, Weights),
+					ActivationValue = activate(WeightedInput, Activation),
+					io:format("[~p] Activated successfully with ~p!~n", [self(), ActivationValue]),
+					lists:map(
+						fun(X) -> X ! {input, self(), ActivationValue} end,
+						OutNeurons),
+					neuron_loop(InNeurons, OutNeurons, Weights, Activation, NewInputValues);
+				_ -> 
+					io:format("Activation of ~p failed!~n", [self()]),
+					not_activated
+			end;
+		{error, OutNeuron, Error} ->
+			io:format("[~p] Received error ~w from ~p!~n", [self(), Error, OutNeuron]),
+			Errors = error_loop(OutNeuron, Error, OutNeurons),
+			case lists:member(stop, Errors) of
+				false -> 
+					WeightedInput = fold_with_weights(InputValues, Weights),
+					MyError = lists:sum(Errors) * derivative(WeightedInput, Activation),
+					io:format("->> Error ~w~n", [MyError]),
+					NewWeights = lists:zipwith(
+						fun(InputValue, Weight) ->
+							Weight - 0.02 * InputValue * MyError
+						end,
+						[1.0 | InputValues], Weights),
+					io:format("->> Updated weights to  ~w~n", [NewWeights]),
+					lists:zipwith(
+						fun(InNeuron, Weight) ->
+							InNeuron ! {error, self(), MyError * Weight}
+						end,
+						InNeurons, tl(Weights)),
+					neuron_loop(InNeurons, OutNeurons, NewWeights, Activation, InputValues);
+				_ -> 
+					io:format("Error estimation of ~p failed!~n", [self()]),
+					not_activated
+			end;
 		{ update, OutNeuron, Value } -> 
 			io:format("Received update ~w from ~p!~n", [Value, OutNeuron])
 	end,
-	neuron_loop(Monitor, InNeurons, OutNeurons, Weights, Activation).
+	neuron_loop(InNeurons, OutNeurons, Weights, Activation, InputValues).
 
 
-init_neuron_loop(Monitor, InNeurons, Activation) -> 
+init_neuron_loop([InputMonitor], _, _, input) -> 
+	receive
+		{out, OutNeurons} -> 
+			synapse_loop(InputMonitor, OutNeurons)
+	end;
+init_neuron_loop(InNeurons, [OutputMonitor], Activation, output) ->
+	Weights = 
+		[0.01 | [
+		rand:normal(0,1) *
+		math:sqrt(2/length(InNeurons)) || _ <- lists:seq(1, length(InNeurons)) ]],
+	output_loop(InNeurons, OutputMonitor, Weights, Activation, 0); 
+init_neuron_loop(InNeurons, _, Activation, hidden) -> 
 	receive
 		{out, OutNeurons} -> 
 			Weights = 
 				[0.01 | [
 				rand:normal(0,1) *
 				math:sqrt(2/length(InNeurons)) || _ <- lists:seq(1, length(InNeurons)) ]],
-			neuron_loop(Monitor, InNeurons, OutNeurons, Weights, Activation)
+			neuron_loop(InNeurons, OutNeurons, Weights, Activation, 0)
 	end.
 
-init_layer(_, _, 0, NeuronPids, _, _) ->
-	NeuronPids;
-init_layer(Node, LayerSize, N, NeuronPids, Activation, PrevNeurons) ->
-	Pid = spawn(Node, deeplearn, init_neuron_loop, [Node, PrevNeurons, Activation]),
-	init_layer(Node, LayerSize, N-1, [Pid | NeuronPids], Activation, PrevNeurons).
+init_layer(_, _, 0, NeuronPids, _, _, _, _) ->
+	lists:reverse(NeuronPids);
+init_layer(Node, LayerSize, N, NeuronPids, Activation, PrevNeurons, OutNeurons, NeuronType) ->
+	Pid = spawn(Node, deeplearn, init_neuron_loop, [PrevNeurons, OutNeurons, Activation, NeuronType]),
+	init_layer(Node, LayerSize, N-1, [Pid | NeuronPids], Activation, PrevNeurons, OutNeurons, NeuronType).
 
 connect_out_neurons([_]) -> ok;
+connect_out_neurons([[], _ | _]) -> ok;
 connect_out_neurons([Layer, InLayer | Tail]) ->
 	lists:map(fun(N) -> N ! {out, Layer} end, InLayer),
 	connect_out_neurons([InLayer | Tail]).
 
 
-spawn_nn_helper(_, [], _, _, OutNeurons, [LastLayer | PrevNeurons]) -> 
+spawn_layers(_, [], _, _, OutNeurons, [LastLayer | PrevNeurons], _) ->
 	lists:map(fun(N) -> N ! {in, OutNeurons} end, LastLayer),
-	connect_out_neurons([OutNeurons, LastLayer | PrevNeurons]),
+	connect_out_neurons([LastLayer | PrevNeurons]),
 	lists:reverse([LastLayer | PrevNeurons]);
-spawn_nn_helper(Node, [H | TailLayers], Activation, InputNeurons, OutNeurons, []) ->
-	NeuronPids = lists:reverse(init_layer(Node, H, H, [], Activation, InputNeurons)),
+spawn_layers(Node, [H | TailLayers], Activation, InputNeurons, OutNeurons, [], NeuronType) ->
+	NeuronPids = init_layer(
+		Node, H, H, [], Activation, InputNeurons, OutNeurons, NeuronType),
 	lists:map(fun(N) -> N ! {out, NeuronPids} end, InputNeurons),
-	spawn_nn_helper(
+	spawn_layers(
 		Node, TailLayers, Activation,
 		InputNeurons, OutNeurons, 
-		[NeuronPids]);
-spawn_nn_helper(Node, [H | TailLayers], Activation, InputNeurons, OutNeurons,
-			    [PrevNeurons | NeuronsBefore]) ->
-	NeuronPids = lists:reverse(init_layer(Node, H, H, [], Activation, PrevNeurons)),
-	spawn_nn_helper(Node, TailLayers, Activation,
+		[NeuronPids], NeuronType);
+spawn_layers(Node, [H | TailLayers], Activation, InputNeurons, OutNeurons,
+			    [PrevNeurons | NeuronsBefore], NeuronType) ->
+	NeuronPids = init_layer(Node, H, H, [], Activation, PrevNeurons, OutNeurons, NeuronType),
+	spawn_layers(Node, TailLayers, Activation,
 					InputNeurons, OutNeurons,
-					[NeuronPids, PrevNeurons | NeuronsBefore]).
+					[NeuronPids, PrevNeurons | NeuronsBefore], NeuronType).
 
-spawn_nn(Node, Layers, Activation, InputNeurons, OutNeurons) ->
-	io:format("Spawning a neural network on ~p with layers ~w....~n", [Node, Layers]),
-	spawn_nn_helper(
+spawn_layers(Node, Layers, Activation, InputNeurons, OutNeurons, NeuronType) ->
+	spawn_layers(
 		Node, Layers, Activation,
-		InputNeurons, OutNeurons, []).
+		InputNeurons, OutNeurons, [], NeuronType).
 
+spawn_nn_helper(Synapses, 
+		 [],
+		 {OutputNode, NOutputs}, Activation,
+		 [LastHiddenBatch | TailNeurons], OutputMinitor) ->
+	% Called for init of the last hidden layer batch, input neurons
+	% are initialized to the last layer of the previous batch.
+	% Output layer is initialized, output neurons of the last hidden batch
+	% are initialized to output layer 
+	% Initialize hidden layers
+	io:format("Initializing output layer...~n", []), 
+	[OutputNeurons] = spawn_layers(
+		OutputNode, [NOutputs], Activation,
+		lists:last(LastHiddenBatch), [OutputMinitor], output),
+	% lists:map(fun(X) -> X ! {out, OutputNeurons} end, lists:last(LastHiddenBatch)),
+	{Synapses, lists:reverse([LastHiddenBatch | TailNeurons]), OutputNeurons};
+spawn_nn_helper(Synapses,
+		 [{HeadNode, HeadLayers} | Tail],
+		 Output, Activation, [], OutputMinitor) ->
+	% Called for init of the first hidden layer batch, input neurons
+	% are initialized to synapses
+	Neurons = spawn_layers(
+		HeadNode, HeadLayers, Activation,
+		Synapses, [], hidden),
+	% io:format("~n!!!Sending ~w, to ~w~n", [hd(Neurons), lists:last(LastHiddenBatch)]),
+	lists:map(fun(X) -> X ! {out, hd(Neurons)} end, Synapses),
+	spawn_nn_helper(
+		Synapses, Tail, 
+		Output, Activation, [Neurons], OutputMinitor);
+spawn_nn_helper(Synapses,
+	     [{HeadNode, HeadLayers} | Tail],
+		 Output, Activation, [LastHiddenBatch | TailNeurons], OutputMinitor) ->
+	% Called for init of the i-th (1 < i < n) hidden layer batch,
+	% input neurons are initialized to the last layer of the previous batch
+	Neurons = spawn_layers(
+		HeadNode, HeadLayers, Activation,
+		lists:last(LastHiddenBatch), [], hidden),
+	% lists:map(fun(X) -> X ! {out, hd(Neurons)} end, lists:last(LastHiddenBatch)),
+	spawn_nn_helper(
+			Synapses, Tail,
+		    Output, Activation,
+		    [Neurons, LastHiddenBatch | TailNeurons], OutputMinitor).
+	
+
+spawn_nn(
+	{InputNode, NInputs}, Hidden, Output, Activation,
+	InputMonitor, OutputMinitor) ->
+	% Initialize input neurons - synapses
+	io:format("Initializing synapses...~n", []),
+	[Synapses] = spawn_layers(
+		InputNode, [NInputs], Activation, [InputMonitor], [], input),
+	io:format("Initializing hidden layers...~n", []), 
+	spawn_nn_helper(Synapses, Hidden, Output, Activation, [], OutputMinitor).
+
+
+% network_monitor_loop(Synapses, OutNeuron) ->
+% 	receive
+% 		{update, synapses, NewSynapses} ->
+% 			network_monitor_loop(NewSynapses, OutNeuron);
+% 		{update, out_neuron, NewOutNeuron} ->
+% 			network_monitor_loop(Synapses, OutNeuron);
+% 		{train, DataSet}
+% 	end.
--- src/test_nn.erl
+++ src/test_nn.erl
@@ -1,17 +1,88 @@
 -module(test_nn).
 
--export([test_activation/0]).
+-export([test_activation/0, nn_monitor/0]).
+
+nn_monitor() ->
+	receive
+		{init, Sender, Synapses, HiddenLayers, OutputNeurons} ->
+			Sender ! {ready, self()},
+			nn_monitor(Synapses, HiddenLayers, OutputNeurons)
+	end,
+	nn_monitor().
+
+
+feed_forward(Neurons, Values) ->
+	lists:zipwith(
+		fun(Neuron, Value) -> 
+			Neuron ! {input, self(), Value}
+		end,
+		Neurons, 
+		Values).
+
+feed_backward(OutputNeurons, Predictions) ->
+	lists:zipwith(
+		fun(OutputNeuron, Prediction) -> 
+			OutputNeuron ! {backpropagate, Prediction}
+		end,
+		OutputNeurons, 
+		Predictions).
+
+receive_prediction(OutputNeurons) ->
+	lists:map(
+		fun(OutputNeuron) ->
+			receive
+				{output, OutputNeuron, Value} ->
+					Value;
+				stop_output -> 
+					io:format("Stoping output loop~n", []),
+					stop
+			end
+		end,
+		OutputNeurons).
+
+nn_monitor(Synapses, HiddenLayers, OutputNeurons) ->
+	receive
+		{train, Xi, Yi} ->
+			feed_forward(Synapses, Xi),
+			_ = receive_prediction(OutputNeurons),
+			feed_backward(OutputNeurons, Yi);
+		{predict, Xi} ->
+			feed_forward(Synapses, Xi),
+			Predictions = receive_prediction(OutputNeurons),
+			io:format("Monitor got prediction: ~w~n", [Predictions])
+	end,
+	nn_monitor(Synapses, HiddenLayers, OutputNeurons).
+	
+	% lists:map(fun(X)->X ! {input, self(), 1.0} end, Synapses),
+	% Prediction = 1.0,
+	% lists:map(
+	% 	fun(X) ->
+	% 		receive
+	% 			{output, X, Activation} ->
+	% 				X ! {backpropagate, Prediction},
+	% 				nn_monitor(Synapses, HiddenLayers, OutputNeurons);
+	% 			A ->
+	% 				io:format(">>> Received ~w ~n", [A])
+	% 		end
+	% 	end,
+	% 	OutputNeurons),
+	% nn_monitor(Synapses, HiddenLayers, OutputNeurons). 
 
 
 test_activation() ->
-	Synapses = deeplearn:init_synapses(2),
-	io:format("~n~nSYNAPSES ~w~n", [Synapses]),
-	Neurons = deeplearn:spawn_nn(node(), [2,3,1], tanh, Synapses, [self()]),
-	io:format("~n~nNEURONS ~w~n", [Neurons]),
-	lists:map(fun(X)->X ! {input, 1.0} end, Synapses),
-	Prediction = 1.0,
+	Monitor = spawn(test_nn, nn_monitor, []),
+	{Synapses, HiddenLayers, OutputNeurons} = deeplearn:spawn_nn(
+		{node(), 2}, 
+		[{node(), [3, 3]}, {node(), [4, 4]}], 
+		{node(), 1}, 
+		tanh, 
+		Monitor, 
+		Monitor),
+	Monitor ! {init, self(), Synapses, HiddenLayers, OutputNeurons},
 	receive
-		{input, _, Activation, WeightedInput} ->
-			Error = (Activation - Prediction) * deeplearn:tanh_prime(WeightedInput),
-			io:format("->> Error ~w~n", [Error])
+		{ready, Monitor} ->
+			Monitor ! {train, [1.0, 1.0], [1.0]}
 	end.
+	
+	% Monitor ! {feed_forward, [1.0, 1.0]},
+	% Monitor ! {feed_backward, [1.0]}.
